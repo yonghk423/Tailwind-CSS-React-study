dbafa9628da84325d09a57ea5c90bad3279c7e70
yonghk423 <yonghk423423@gmail.com>
2022-09-20 21:45:05 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Main />
     </div>
   );
 }
