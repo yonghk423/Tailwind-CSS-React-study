ac2a1ba8f4d889390e7c448af88dbcb9515930ed
yonghk423 <yonghk423423@gmail.com>
2022-09-21 13:29:02 +0900

    study

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Main from './components/Main';
 import HeadlineCards from './components/HeadlineCards';
+import Food from './components/Food';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Main />
       <HeadlineCards />
+      <Food />
     </div>
   );
 }
