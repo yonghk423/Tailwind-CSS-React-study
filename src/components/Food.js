28524b43dd3212fbc027521ccf7d75c3a53105f1
yonghk423 <yonghk423423@gmail.com>
2022-09-21 15:07:25 +0900

    study

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import { data } from '../data/data'
 const Food = () => {
-    console.log(data);
+    // console.log(data);
     const [foods, setFoods] = useState(data);
+
+    // Filter Type burgers/pizza/etc
+    const filterType = (category) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === category;
+            })
+        )
+    }
+    // Filter by price 
+        const filterPrice = (price) => {
+            setFoods(
+                data.filter((item) => {
+                    return item.price === price;
+                })
+            )
+        }
     return (
         <div className='max-w-[1640px] m-auto px-4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -13,11 +30,21 @@ const Food = () => {
                 <div>
                     <p className='font-bold text-gray-700'>Filter Type</p>
                     <div className='flex justify-between flex-wrap'>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chickens</button>
+                        <button 
+                            onClick={() => setFoods(data)} 
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+                        <button 
+                            onClick={() => filterType('burger')} 
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+                        <button 
+                            onClick={() => filterType('pizza')} 
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
+                        <button 
+                            onClick={() => filterType('salad')} 
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
+                        <button 
+                            onClick={() => filterType('chicken')} 
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chickens</button>
                     </div>
                 </div>
 
