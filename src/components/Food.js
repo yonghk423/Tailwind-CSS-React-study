2fc1e1730009873595971e637075f0d4e5fb2109
yonghk423 <yonghk423423@gmail.com>
2022-09-21 14:51:23 +0900

    study

--- src/components/Food.js
+++ src/components/Food.js
@@ -35,14 +35,14 @@ const Food = () => {
             {/* Display foods */}
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
                 {foods.map((item, index) => (
-                    <div key={index} className='border shadow-lg hover:scale-105 duration-300'>
-                        <img src={item.image} alt='' />
-                        <div>
-                        <p>{item.name}</p>
-                        <p>
-                            <span>{item.price}</span>
-                        </p>
-                    </div>
+                    <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+                        <img className='w-full h-[200px] object-cover rounded-t-lg' src={item.image} alt='' />
+                        <div className='flex justify-between px-2 py-4'>
+                            <p className='font-bold'>{item.name}</p>
+                            <p>
+                                <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
+                            </p>
+                        </div>
                     </div>                    
                 ))}
             </div>
